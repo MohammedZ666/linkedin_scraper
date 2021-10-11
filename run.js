63ef0c6af38b5237a53a9fb269fb645de72e92c8
Abu Obaida Zishan <abuobaidazishan@pop-os.localdomain>
2021-10-11 10:55:01 +0600

    fixed bugs

--- run.js
+++ run.js
@@ -5,8 +5,8 @@ const path = require('path');
 
 const query = 'site:linkedin.com/in/ AND "javascript developer" AND "BRAC University"'
 const countryCode = "bd"; //two letter country code
-const maxPage = 1;   // no of google search pages to scrape
-const sessionCookieValue = '';
+const profileMaxCount = 12;   // no of google search pages to scrape
+const sessionCookieValue = 'AQEDATVRtXUC46DyAAABfGq6KdUAAAF8jsat1VYArrnEv301Dw5Wpli9YisDa0_ZPKQb_xuUgkN0XKLrgkHMKLDRpXnINYZ7doRAO4N5BwKlYI1fIDIwSFZJFLcaPKfG7ueq35QK2r1mVixM__27JD78';
 // Prepare the scraper
 // Loading it in memory
 
@@ -21,19 +21,31 @@ const sessionCookieValue = '';
     await page.type('input[name="q"]', query);
     await page.keyboard.press('Enter');
     const profileLinks = [];
-    for (let i = 0; i < maxPage; i++) {
+    let profileScraped = 0;
+    while (profileScraped < profileMaxCount) {
         await page.waitForSelector(`a[href^="https://${countryCode}.linkedin.com/in/"]`)
-        profileLinks.push(...await page.$$eval(`a[href^="https://${countryCode}.linkedin.com/in/"]`, el => el.map(x => x.getAttribute("href"))));
+        let currentLinks = await page.$$eval(`a[href^="https://${countryCode}.linkedin.com/in/"]`, el => el.map(x => x.getAttribute("href")))
+        profileLinks.push(...currentLinks);
+        profileScraped += currentLinks.length;
         await page.click("#pnnext")
-
+    }
+    console.log("Total profiles scraped => ", profileScraped)
+    //Deleting excess links
+    if (profileScraped > profileMaxCount) {
+        let indexDelFrom = profileMaxCount
+        profileLinks.splice(indexDelFrom)
     }
     await browser.close();
-    console.log(profileLinks.length)
-    console.log(profileLinks)
+    console.log("Total profiles demanded => ", profileLinks.length)
+    console.log("Profile links => ", profileLinks)
     const scraper = new LinkedInProfileScraper({
         sessionCookieValue: sessionCookieValue,
         keepAlive: false
     });
+    const saveDir = path.join(__dirname, "profiles", "")
+    if (!fs.existsSync(saveDir)) {
+        fs.mkdirSync('profiles', { recursive: true });
+    }
 
     for (let i = 0; i < profileLinks.length; i++) {
         await scraper.setup()
@@ -47,9 +59,10 @@ const sessionCookieValue = '';
             continue;
         }
         //console.log(profileJson)
-        fs.writeFileSync(path.join(__dirname, "profiles", `${link.substring(28)}.json`), JSON.stringify(profileJson));
+
+        fs.writeFileSync(`${saveDir}/${link.substring(28)}.json`, JSON.stringify(profileJson));
+        console.log(`Profiles saved at ${saveDir}`)
     };
     // other actions...
     //await browser.close();
 })();
-
