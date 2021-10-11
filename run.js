0737e15ee79b90577240f8fc78ae865f0e7582c8
Abu Obaida Zishan <abuobaidazishan@pop-os.localdomain>
2021-10-11 10:03:13 +0600

    prototype ready

--- run.js
+++ run.js
@@ -1,17 +1,55 @@
-// Plain Javascript
 const { LinkedInProfileScraper } = require('linkedin-profile-scraper');
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+
+const query = 'site:linkedin.com/in/ AND "javascript developer" AND "BRAC University"'
+const countryCode = "bd"; //two letter country code
+const maxPage = 1;   // no of google search pages to scrape
+const sessionCookieValue = '';
+// Prepare the scraper
+// Loading it in memory
 
 (async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        channel: "chrome"
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com/', { timeout: 60 * 1000 });
+    await page.waitForSelector('input[name="q"]')
+    await page.type('input[name="q"]', query);
+    await page.keyboard.press('Enter');
+    const profileLinks = [];
+    for (let i = 0; i < maxPage; i++) {
+        await page.waitForSelector(`a[href^="https://${countryCode}.linkedin.com/in/"]`)
+        profileLinks.push(...await page.$$eval(`a[href^="https://${countryCode}.linkedin.com/in/"]`, el => el.map(x => x.getAttribute("href"))));
+        await page.click("#pnnext")
+
+    }
+    await browser.close();
+    console.log(profileLinks.length)
+    console.log(profileLinks)
     const scraper = new LinkedInProfileScraper({
-        sessionCookieValue: 'AQEDATVRtXUFNTdpAAABfAz7VgsAAAF8fV4gF00ADwKjzSgyoUSMirkJ_doPYxC0F-ifv8mcmhryDWLVxDc9haFhGQsOEa-VOo17bVN9rxyeu0_ca68PBuQ32LDL2DHlOe6zsgw9bJiNM5A5yeScjgZO',
+        sessionCookieValue: sessionCookieValue,
         keepAlive: false
     });
 
-    // Prepare the scraper
-    // Loading it in memory
-    await scraper.setup()
+    for (let i = 0; i < profileLinks.length; i++) {
+        await scraper.setup()
+        let link = profileLinks[i];
+        link = link.replace(countryCode, "www")
+        let profileJson;
+        try {
+            profileJson = await scraper.run(link)
+        } catch (error) {
+            console.log(error)
+            continue;
+        }
+        //console.log(profileJson)
+        fs.writeFileSync(path.join(__dirname, "profiles", `${link.substring(28)}.json`), JSON.stringify(profileJson));
+    };
+    // other actions...
+    //await browser.close();
+})();
 
-    const result = await scraper.run('https://www.linkedin.com/search/results/people/?keywords=javascript%20developer&origin=SWITCH_SEARCH_VERTICAL&sid=IIQ')
-
-    console.log(result)
-})()
